a5426534d4a19c2904be4d59a00b097caf6b2868
web <webdev2021@gmail.com>
2021-04-03 07:45:03 +0300

    api video from array

--- src/componets/slickplaylists/slickplaylists.js
+++ src/componets/slickplaylists/slickplaylists.js
@@ -6,11 +6,11 @@ const Slickplaylists= (props) => {
   <div className="container">
 	      <div className="row">
        <div className="play-list-names col-md-12 col-sm-12 slick-slider">
-       {props.playlists.map((item)=>{
+       {props["playlists"][0].playlists.map((item)=>{
 
     
             return <div className="item " style={{"width":"100%","display":"inline-block"}}>
-                <a href={item.link} tabIndex="-1">{item.playlistname}</a>
+            <a href={itemlink} tabIndex="-1">{item.name}</a>
             </div>
            })}
        </div>
